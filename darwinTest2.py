fbb78b3aee9b43872b73a103bacd9a58c2160c6a
Sherchi <darwinliao@yahoo.ca>
2023-12-07 21:26:20 -0500

    Prints out 10 of each cluster for analysis

--- darwinTest2.py
+++ darwinTest2.py
@@ -8,10 +8,11 @@ from sklearn.manifold import TSNE
 
 
 # Load the JSON file
-with open('CleanedDataRP_Modified.json', 'r') as file:
+with open('CleanedDataRPnoSyntax.json', 'r') as file:
     data = json.load(file)
 
 feature_vectors = []
+feature_vectors_words = []
 
 # Function to extract features around a given index
 def extract_features(tags, index, window_size=3):
@@ -22,12 +23,15 @@ def extract_features(tags, index, window_size=3):
 # Iterate over each sentence in the data
 for item in data:
     tags = item["morphology"].split()
+    words = item["text"].split()
 
     # Find instances of the predicate (RP) and create feature vectors
     for i, tag in enumerate(tags):
         if tag == 'RP':
             feature_vector = extract_features(tags, i)
             feature_vectors.append(feature_vector)
+            feature_words = extract_features(words, i)
+            feature_vectors_words.append(feature_words)
 
 # Define window size and padding tag
 window_size = 7  # This is the total size of the feature vector
@@ -41,7 +45,11 @@ def pad_and_extract_features(vector, window_size, padding_tag):
 
 for i in range(len(feature_vectors)):
     feature_vectors[i] = pad_and_extract_features(feature_vectors[i],window_size,padding_tag)
+    feature_vectors_words[i] = pad_and_extract_features(feature_vectors_words[i],window_size,padding_tag)
 
+# max_padding_count = 3  # Adjust as needed
+# feature_vectors = [vector for vector in feature_vectors if vector.count(padding_tag) < max_padding_count]
+# feature_vectors_words = [vector for vector in feature_vectors_words if vector.count(padding_tag) < max_padding_count]
 
 # Convert the array of POS tag arrays into text strings
 pos_tags_corpus_text = [' '.join(tags) for tags in feature_vectors]
@@ -80,6 +88,27 @@ cluster_labels = kmeans.fit_predict(euclideanDistances)
 for i, label in enumerate(cluster_labels):
     print(f"Sentence {i+1}: Cluster {label}")
 
+# Initialize empty lists to store sentences in each cluster
+cluster_sentences_tags = [[] for _ in range(num_clusters)]
+cluster_sentences_words = [[] for _ in range(num_clusters)]
+
+# Iterate through sentences and cluster labels
+for i, label in enumerate(cluster_labels):
+    cluster_sentences_tags[label].append(feature_vectors[i])  # Append the POS tag sequence
+    cluster_sentences_words[label].append(feature_vectors_words[i])
+
+# Print the first 10 elements in each cluster for both tags and words
+for cluster_id, (sentences_tags, sentences_words) in enumerate(zip(cluster_sentences_tags, cluster_sentences_words)):
+    print(f"Cluster {cluster_id} - {len(sentences_tags)} sentences (Tags):")
+    for sentence in sentences_tags[100:110]:
+        print(sentence)
+    print("\n")
+
+    print(f"Cluster {cluster_id} - {len(sentences_words)} sentences (Words):")
+    for sentence in sentences_words[100:110]:
+        print(sentence)
+    print("\n")
+
 # Reduce the dimensionality using t-SNE
 tsne = TSNE(n_components=2, perplexity=30, random_state=0)  # You can adjust perplexity and other parameters
 bow_matrix_tsne = tsne.fit_transform(euclideanDistances)
@@ -91,6 +120,7 @@ plt.xlabel('t-SNE Component 1')
 plt.ylabel('t-SNE Component 2')
 plt.colorbar(label='Cluster')
 plt.show()    
+
 # # Reduce the dimensionality using PCA
 # pca = PCA(n_components=2)  # You can choose 2 or 3 components for 2D or 3D visualization
 # bow_matrix_pca = pca.fit_transform(bow_matrix_dense)
